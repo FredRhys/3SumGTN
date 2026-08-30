a0d7ba995fc645785083b7218863c2e96ad10d61
Frederick Wright <fredrhys@gmail.com>
2026-08-30 14:34:44 +0100

    Removed vestiges of addPoly and subPoly

--- src/poly.h
+++ src/poly.h
@@ -12,8 +12,6 @@ typedef struct Poly{
 }Poly;
 
 Poly makePoly(uint64_t deg2, uint64_t deg1, uint64_t deg0);
-// Poly addPoly(Poly augend, Poly addend, uint64_t modulus);
-// Poly subPoly(Poly minuend, Poly subtrahend, uint64_t modulus);
 Poly mulPoly(Poly multiplier, Poly multiplicand, ModEntry modEntry, uint64_t int_6k);
 Poly mulPolyConst(Poly multiplier, uint64_t multiplicand, ModEntry modEntry);
 Poly getMonicPoly(Poly operand, ModEntry modEntry);
