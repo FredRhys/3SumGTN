1353416da69ed63131641e4e64aaa475d38f7b67
Frederick Wright <fredrhys@gmail.com>
2026-09-02 13:41:49 +0100

    Added primeIterator

--- src/advanced.c
+++ src/advanced.c
@@ -13,5 +13,9 @@ PrimeWrapper makePrimeWrapper(uint64_t prime, PrimeWrapper* prev) {
 }
 
 bool tryAdvanced(uint64_t k) {
+    primesieve_iterator primeIterator;
+    primesieve_init(&primeIterator);
+    
+    primesieve_free_iterator(&primeIterator);
     return false;
 }
