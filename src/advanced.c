1dd4ef8a252ab92f2f87855f92993d24d34774dd
Frederick Wright <fredrhys@gmail.com>
2026-09-02 13:44:13 +0100

    Implemented skeleton of tryAdvanced

--- src/advanced.c
+++ src/advanced.c
@@ -12,10 +12,32 @@ PrimeWrapper makePrimeWrapper(uint64_t prime, PrimeWrapper* prev) {
     return (PrimeWrapper){prime, NULL, NULL, prev};
 }
 
+bool trySmallPowersOfSmallPrimes() {
+    return false;
+}
+
+bool tryLargePowersOfSmallPrimes() {
+    return false;
+}
+
+bool tryLargePrimes() {
+    return false;
+}
+
 bool tryAdvanced(uint64_t k) {
     primesieve_iterator primeIterator;
     primesieve_init(&primeIterator);
     
+    if (trySmallPowersOfSmallPrimes()) {
+        return true;
+    }
+    if (tryLargePowersOfSmallPrimes()) {
+        return true;
+    }
+    if (tryLargePrimes()) {
+        return true;
+    }
+
     primesieve_free_iterator(&primeIterator);
     return false;
 }
