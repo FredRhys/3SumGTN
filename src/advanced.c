60bad80ac61f165469f4fda315743fa7ddec0f95
Frederick Wright <fredrhys@gmail.com>
2026-09-02 13:40:06 +0100

    Made constructors for wrappers

--- src/advanced.c
+++ src/advanced.c
@@ -1,5 +1,17 @@
 #include "advanced.h"
 
+ResidueWrapper makeResidueWrapper(uint64_t residue) {
+    return (ResidueWrapper){residue, NULL};
+}
+
+ModEntryWrapper makeModEntryWrapper(ModEntry modEntry, ModEntryWrapper* prev) {
+    return (ModEntryWrapper){modEntry, NULL, prev};
+}
+
+PrimeWrapper makePrimeWrapper(uint64_t prime, PrimeWrapper* prev) {
+    return (PrimeWrapper){prime, NULL, NULL, prev};
+}
+
 bool tryAdvanced(uint64_t k) {
     return false;
 }
