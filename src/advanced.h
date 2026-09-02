c3d0ac24044e4c74a57c61a1b9fbe4dc46c3b4f8
Frederick Wright <fredrhys@gmail.com>
2026-09-02 13:35:08 +0100

    Made structs for advanced algorithm

--- src/advanced.h
+++ src/advanced.h
@@ -11,6 +11,24 @@
 #define DIVBOUND 545094
 #define SQRT_DIVBOUND 739
 
+typedef struct ResidueWrapper {
+    uint64_t residue;
+    struct ResidueWrapper* next; // added in incidence order
+}ResidueWrapper;
+
+typedef struct ModEntryWrapper {
+    ModEntry modEntry;
+    ResidueWrapper* residueHead;
+    struct ModEntryWrapper* prev; // the ModEntryWrapper head should be that with the greatest modulus.
+}ModEntryWrapper;
+
+typedef struct PrimeWrapper {
+    uint64_t prime;
+    ModEntryWrapper* firstModEntry;
+    ModEntryWrapper* lastModEntry;
+    struct PrimeWrapper* prev; // the PrimeWrapper head should be that with the greatest modulus.
+}PrimeWrapper;
+
 // global variables
 extern FILE* resultsDotTxt;
 
