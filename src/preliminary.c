3edc2b83e30c4de9b94a08fa21e5966235daaa81
Frederick Wright <fredrhys@gmail.com>
2026-09-11 20:33:31 +0100

    Tweaked integer signedness

--- src/preliminary.c
+++ src/preliminary.c
@@ -1,6 +1,6 @@
 #include "preliminary.h"
 
-static bool isSolution(uint64_t x,  uint64_t y, uint64_t radicand, uint64_t k) {
+static bool isSolution(int64_t x,  int64_t y, int64_t radicand, uint64_t k) {
     if ((uint64_t)x * x * x - x != radicand) {return false;}
     fprintfResults(x, y, y, k);
     return true;
@@ -10,7 +10,7 @@ bool tryPreliminary(uint64_t k) {
     const uint64_t _6k = 6 * k;
     int64_t x, radicand;
     for (int64_t y = -2 * sqrtl(k); y > -UINT21_MAX; y--) {
-        radicand = _6k + 2 * (y - (int64_t)y * y * y);
+        radicand = _6k + 2 * y * (1 - y * y);
         x = cbrtl(radicand);
         if (isSolution(x, y, radicand, k)) {return true;}
         if (isSolution(x+1, y, radicand, k)) {return true;}        
