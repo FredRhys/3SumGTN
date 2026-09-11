aba2afe45895b8d3fb6340fcfeb2f5acd2f9f987
Frederick Wright <fredrhys@gmail.com>
2026-09-11 18:20:19 +0100

    Implemented preliminary algorithm.

--- src/preliminary.c
+++ src/preliminary.c
@@ -1,5 +1,19 @@
 #include "preliminary.h"
 
+bool isSolution(uint64_t x,  uint64_t y, uint64_t radicand, uint64_t k) {
+    if ((uint64_t)x * x * x - x != radicand) {return false;}
+    fprintfResults(x, y, y, k);
+    return true;
+}
+
 bool tryPreliminary(uint64_t k) {
+    const uint64_t _6k = 6 * k;
+    int64_t x, radicand;
+    for (int64_t y = -2 * sqrtl(k); y > -UINT21_MAX; y--) {
+        radicand = _6k + 2 * (y - (uint64_t)y * y * y);
+        x = cbrtl(radicand);
+        if (isSolution(x, y, radicand, k)) {return true;}
+        if (isSolution(x+1, y, radicand, k)) {return true;}        
+    }
     return false;
 }
