fc635c7d7c431b036af0bd4cfadedf87996a6232
Frederick Wright <fredrhys@gmail.com>
2026-09-11 18:19:55 +0100

    fixed compilation issues.

--- src/formula.c
+++ src/formula.c
@@ -51,14 +51,8 @@ int64_t adjustSolutions(int64_t operand) {
 	return (operand == 0 || operand == 1) ? 0 : operand + 1;
 }
 
-void fprintResults(uint64_t x, uint64_t y, uint64_t z, uint64_t k) {
+void fprintfResults(uint64_t x, uint64_t y, uint64_t z, uint64_t k) {
 	z = adjustSolutions(z);
-	int64_t x = (divisor+formulaSqrt)/2;
-	int64_t y = ((int64_t)divisor - (int64_t)formulaSqrt)/2;
-	if (dividend > 0) {
-		x = -x;
-		y = -y;
-	}
 	x = adjustSolutions(x);
 	y = adjustSolutions(y);
 	(void)fprintf(resultsDotTxt, "%ld : %ld / %ld / %ld\n", k, x, y, z);
@@ -71,6 +65,13 @@ bool checkFormulaResults(__int128_t dividend, uint64_t divisor, uint64_t _3d, ui
 	if ((divisor & 1) != (sqrtand & 1)) {return false;}
 	uint64_t formulaSqrt;
     if (!iSqrt128(sqrtand, &formulaSqrt)) {return false;}
+
+	int64_t x = (divisor+formulaSqrt)/2;
+	int64_t y = ((int64_t)divisor - (int64_t)formulaSqrt)/2;
+	if (dividend > 0) {
+		x = -x;
+		y = -y;
+	}
 	(void)fprintfResults(x, y, z, k);	
 	return true;
 }
--- src/formula.h
+++ src/formula.h
@@ -11,7 +11,7 @@
 extern FILE* resultsDotTxt;
 
 __uint128_t abs128(__int128_t operand);
-void fprintResults(uint64_t x, uint64_t y, uint64_t z, uint64_t k)
+void fprintfResults(uint64_t x, uint64_t y, uint64_t z, uint64_t k);
 bool checkFormulaResults(__int128_t dividend, uint64_t divisor, uint64_t _3d, uint64_t k, int64_t z);
 
 #endif
