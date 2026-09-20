3bd48f332f44419e0e78f12490a07063f096f12e
Frederick Wright <fredrhys@gmail.com>
2026-09-20 10:04:48 +0100

    Comment-out toughs and tryPreliminary

--- src/main.c
+++ src/main.c
@@ -16,15 +16,15 @@ void createResultsTxt() {
 }
 
 void mainloop(uint64_t min, uint64_t max, uint64_t threads, int64_t minDivbound, int64_t maxDivbound) {
-  uint64_t toughs[9] = {64507, 28397, 80502, 68063, 44293, 91163, 72682, 99377, 72717};
+  //uint64_t toughs[9] = {64507, 28397, 80502, 68063, 44293, 91163, 72682, 99377, 72717};
   #pragma omp parallel for num_threads(threads)
-  for (uint8_t i = 0; i <= 8; i++) {
-    //if (tryBasic(i)) {continue;}
+  for (uint64_t i = min; i <= max; i++) {
+  if (tryBasic(i)) {continue;}
     for (int64_t j = minDivbound; j <= maxDivbound; j *= 2) {
-      if (tryAdvanced(toughs[i], j)) {goto mainloop_continue;}
+      if (tryAdvanced(i, j)) {goto mainloop_continue;}
     }
     //if (tryPreliminary(i)) {continue;}
-    (void)fprintf(resultsDotTxt, "Fail: %"PRIu64"\n", toughs[i]);
+    (void)fprintf(resultsDotTxt, "Fail: %"PRIu64"\n", i);
 mainloop_continue:
   }
 }
