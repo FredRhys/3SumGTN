708aa80a00f18ed7b58b41025124b49ebf0dc867
Frederick Wright <fredrhys@gmail.com>
2026-09-23 08:05:50 +0100

    Implment isQr for speedup

---
+++ src/isQr.c
@@ -0,0 +1,61 @@
+#include "isQr.h"
+
+bool isQrMod5(uint64_t operand) {
+  switch (operand % 5) {
+    case 0:
+    case 1:
+    case 4:
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool isQrMod7(uint64_t operand) {
+  switch (operand % 7) {
+    case 0:
+    case 1:
+    case 2:
+    case 4:
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool isQrMod11(uint64_t operand) {
+  switch (operand % 11) {
+    case 0:
+    case 1:
+    case 3:
+    case 4:
+    case 5:
+    case 9:
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool isQrMod13(uint64_t operand) {
+  switch (operand % 13) {
+    case 0:
+    case 1:
+    case 3:
+    case 4:
+    case 9:
+    case 10:
+    case 12:
+      return true;
+    default:
+      return false;
+  }
+}
+
+bool isQrModSmallPrimes(uint64_t operand) {
+  if (!isQrMod5(operand)) {return false;}
+  if (!isQrMod7(operand)) {return false;}
+  if (!isQrMod11(operand)) {return false;}
+  if (!isQrMod13(operand)) {return false;}
+  return true;
+}
---
+++ src/isQr.h
@@ -0,0 +1,9 @@
+#ifndef IS_QR_H
+#define IS_QR_H
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+bool isQrModSmallPrimes(uint64_t operand);
+
+#endif
