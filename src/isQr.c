4e22317207bba0cf0a817a34c72f9087f87106ed
Frederick Wright <fredrhys@gmail.com>
2026-09-25 07:34:47 +0100

    Exhaust switch statements

--- src/isQr.c
+++ src/isQr.c
@@ -6,6 +6,9 @@ bool isQrMod5(uint64_t operand) {
     case 1:
     case 4:
       return true;
+    case 2:
+    case 3:
+      return false;
     default:
       return false;
   }
@@ -18,6 +21,10 @@ bool isQrMod7(uint64_t operand) {
     case 2:
     case 4:
       return true;
+    case 3:
+    case 5:
+    case 6:
+      return false;
     default:
       return false;
   }
@@ -32,6 +39,12 @@ bool isQrMod11(uint64_t operand) {
     case 5:
     case 9:
       return true;
+    case 2:
+    case 6:
+    case 7:
+    case 8:
+    case 10:
+      return false;
     default:
       return false;
   }
@@ -47,6 +60,13 @@ bool isQrMod13(uint64_t operand) {
     case 10:
     case 12:
       return true;
+    case 2:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 11:
+      return false;
     default:
       return false;
   }
